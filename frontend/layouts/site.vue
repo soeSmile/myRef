b7b7862f96c713b973ff4ec54ca5bbc137f72b30
Smirnov <soe190715@gmail.com>
2021-04-16 00:01:12 +0700

    init project

--- frontend/layouts/site.vue
+++ frontend/layouts/site.vue
@@ -1,45 +1,60 @@
 <template>
   <v-app>
-    <v-app-bar app flat>
-      <v-app-bar-nav-icon/>
-      <v-toolbar-title>Page title</v-toolbar-title>
+    <v-app-bar app dark
+               extension-height="80"
+               color="blue darken-4">
+      <v-container class="py-0 fill-height">
 
-      <v-text-field flat
-                    hide-details
-                    label="Search"
-                    prepend-inner-icon="mdi-magnify"
-                    class="ma-4"
-                    clearable
-                    append-outer-icon="mdi-send"
-                    solo
-                    @click:append-outer="search"/>
+        <v-avatar class="mr-10"
+                  color="grey darken-1"
+                  size="32"/>
+        <v-toolbar-title>MyRef.top</v-toolbar-title>
 
-      <v-btn text>
-        <v-icon left>mdi-account</v-icon>
-        User Name
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-login-variant</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-logout-variant</v-icon>
-      </v-btn>
+        <v-select hide-no-data
+                  hide-details
+                  solo-inverted
+                  flat
+                  clearable
+                  class="mx-4"
+                  label="Катеории"
+                  v-model="selectCategory"
+                  :items="categories"
+                  item-text="name"
+                  item-value="id"/>
 
-      <template v-slot:extension>
-        <v-tabs centered
-                color="grey darken-1">
-          <v-tab v-for="link in links" :key="link">
-            {{ link }}
-          </v-tab>
-        </v-tabs>
-      </template>
+        <v-spacer/>
+
+        <v-btn dark text>
+          <v-icon left>mdi-cloud-search-outline</v-icon>
+          Advanced Search
+        </v-btn>
+
+
+        <v-menu offset-y>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn dark text
+                   v-bind="attrs"
+                   v-on="on">
+              <v-icon left>mdi-account</v-icon>
+              User Name
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="(item, index) in items" :key="index">
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+
+      </v-container>
+
     </v-app-bar>
 
     <v-main>
       <nuxt/>
     </v-main>
 
-    <v-footer padless>
+    <v-footer padless fixed>
       <v-col class="text-center" cols="12">
         {{ new Date().getFullYear() }} — <strong>myRef.top</strong>
       </v-col>
@@ -61,11 +76,35 @@ export default {
 
   data() {
     return {
-      links: [
-        'Dashboard',
-        'Messages',
-        'Profile',
-        'Updates',
+      loading       : false,
+      categories    : [
+        {id: 1, name: 'Новости'},
+        {id: 2, name: 'Наука'},
+        {id: 3, name: 'Техника'},
+        {id: 4, name: 'Бизнес и Работа'},
+        {id: 5, name: 'Образование'},
+        {id: 6, name: 'Медицина'},
+        {id: 7, name: 'Юмор'},
+        {id: 8, name: 'Еда'},
+        {id: 9, name: 'Хобби и Досуг'},
+        {id: 10, name: 'Музыка'},
+        {id: 11, name: 'Кино'},
+        {id: 12, name: 'Путешествия'},
+        {id: 13, name: 'Покупки'},
+        {id: 14, name: 'Домоводство'},
+        {id: 15, name: 'Животные'},
+        {id: 16, name: 'Дети'},
+        {id: 17, name: 'Религия и Эзотерика'},
+        {id: 18, name: 'Личное'},
+        {id: 19, name: 'Посмотреть позже'}
+      ],
+      selectCategory: null,
+
+      items: [
+        {title: 'Click Me'},
+        {title: 'Click Me'},
+        {title: 'Click Me'},
+        {title: 'Click Me 2'},
       ],
     }
   },
@@ -74,9 +113,6 @@ export default {
 
   watch: {},
 
-  methods: {
-    search() {
-    }
-  }
+  methods: {}
 }
 </script>
