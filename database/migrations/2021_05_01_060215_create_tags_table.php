b1c4e355b3124a9fa949dd71f5fa7f54b73cae3f
soeSmile <soe190715@gmail.com>
2021-05-01 13:11:03 +0700

    tmplRef

---
+++ database/migrations/2021_05_01_060215_create_tags_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+
+        DB::statement('CREATE INDEX tags_name_idx ON tags USING gin (name gin_trgm_ops)');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
