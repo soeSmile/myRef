25ab5a130d42f41c5eccbb354b1a1f77c92d3704
soeSmile <soe190715@gmail.com>
2021-06-21 22:44:31 +0700

    tmplRef

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -8,7 +8,10 @@ export default {
         meta         : [
             {charset: 'utf-8'},
             {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-            {hid: 'description', name: 'description', content: 'Meta description'}
+            {hid: 'description', name: 'description', content: ''}
+        ],
+        link         : [
+            {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
         ]
     },
 
@@ -41,7 +44,10 @@ export default {
         '~/plugins/builder',
         '~/plugins/mixin',
         '~/plugins/helper',
+        '~/plugins/vuesax'
     ],
+
+    components: true,
 
     modules: [
         '@nuxtjs/axios',
