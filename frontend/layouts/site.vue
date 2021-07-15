3fb6952941da3f9dd1a12bbf7bd7103507fdf85f
soeSmile <soe190715@gmail.com>
2021-07-16 03:30:33 +0700

    tmplRef

--- frontend/layouts/site.vue
+++ frontend/layouts/site.vue
@@ -1,26 +1,8 @@
 <template>
-  <div class="center examplex">
-    <vs-navbar center-collapsed square v-model="active">
-      <template #left>
-      </template>
-      <vs-navbar-item :active="active === 'guide'" id="guide">
-        Guide
-      </vs-navbar-item>
-      <vs-navbar-item :active="active === 'docs'" id="docs">
-        Documents
-      </vs-navbar-item>
-      <vs-navbar-item :active="active === 'components'" id="components">
-        Components
-      </vs-navbar-item>
-      <vs-navbar-item :active="active === 'license'" id="license">
-        license
-      </vs-navbar-item>
-      <template #right>
-        <vs-button flat>Login</vs-button>
-        <vs-button flat>Get Started</vs-button>
-      </template>
-    </vs-navbar>
-
+  <div>
+    1
+    2
+    3
   </div>
 </template>
 
