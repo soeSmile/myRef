7749c95d0c681f1d05ae1766576929704eec7d6f
Smirnov <soe190715@gmail.com>
2021-07-17 18:15:05 +0700

    ui

---
+++ frontend/components/site/header/siteHeader.vue
@@ -0,0 +1,56 @@
+<template>
+  <nav class="sm-nav">
+    <div class="sm-nav-start">
+      <span class="sm-fnt bold size-2">
+        MyRef.top
+      </span>
+    </div>
+    <div class="sm-nav-center"></div>
+    <div class="sm-nav-end">
+      <n-link to="/login"
+              class="sm-nav-item">
+        <i class='bx bx-exit'></i>
+        <span class="sm-ml-2 sm-fnt light">
+          {{ login }}
+        </span>
+      </n-link>
+
+      <span class="sm-ml-1 sm-mr-1">|</span>
+
+      <n-link to="/register"
+              class="sm-nav-item">
+        <i class='bx bxs-key'></i>
+        <span class="sm-ml-2 sm-fnt light">
+          {{ register }}
+        </span>
+      </n-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "siteHeader",
+
+  created() {
+  },
+
+  mounted() {
+  },
+
+  props: {},
+
+  data() {
+    return {
+      login   : 'Вход',
+      register: 'Регистрация',
+    }
+  },
+
+  computed: {},
+
+  watch: {},
+
+  methods: {}
+}
+</script>
--- frontend/layouts/site.vue
+++ frontend/layouts/site.vue
@@ -1,20 +1,32 @@
 <template>
-  <div>
-    1
-    2
-    3
-  </div>
+  <section class="sm-layout">
+    <header class="sm-layout-header sm-bg-color-1 sm-color-white sm-p-4">
+      <div class="sm-container center">
+        <site-header/>
+      </div>
+    </header>
+    <section class="sm-layout-content">
+      Content
+    </section>
+    <footer class="sm-layout-footer">
+      Footer
+    </footer>
+  </section>
 </template>
 
 <script>
 
+import siteHeader from "../components/site/header/siteHeader";
+
 export default {
   name: "site",
 
+  components: {
+    siteHeader
+  },
+
   data() {
-    return {
-      active: 'guide'
-    }
+    return {}
   }
 }
 </script>
