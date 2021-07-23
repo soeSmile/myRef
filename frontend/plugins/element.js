daaf3c69fd0eafafdb0103273986e836f39b9389
Smirnov <soe190715@gmail.com>
2021-07-23 14:58:29 +0700

    ui

--- frontend/plugins/element.js
+++ frontend/plugins/element.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui';
 import locale from 'element-ui/lib/locale/lang/ru-RU'
-import './element-theme/index.css';
 
 Vue.use(ElementUI, {locale})
