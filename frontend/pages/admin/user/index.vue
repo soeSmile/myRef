c3f1f63b2351d521c7396187105a21a96e059f51
Smirnov <soe190715@gmail.com>
2021-07-25 16:55:29 +0700

    ui

--- frontend/pages/admin/user/index.vue
+++ frontend/pages/admin/user/index.vue
@@ -1,20 +1,49 @@
 <template>
-  <div>
-    <nav class="sm-nav sm-bg-color-4 sm-color-color-3">
+  <section>
+    <nav class="sm-nav sm-bg-color-10 sm-color-color-9">
       <div class="sm-nav-start">
-        <div class="sm-nav-item sm-p-4 sm-link-hover sm-hover-color-4 sm-hover-bg-color-3"
+        <div class="sm-nav-item sm-p-4 sm-link-hover sm-hover-color-10 sm-hover-bg-color-9"
              @click="getAll()">
           <i class="mdi mdi-reload sm-mr-1"></i>
           <span>Reload</span>
         </div>
         <n-link to="/admin/user/new"
-                class="sm-nav-item sm-p-4 sm-link sm-hover-color-4 sm-hover-bg-color-3">
+                class="sm-nav-item sm-p-4 sm-link sm-hover-color-10 sm-hover-bg-color-9">
           <i class="mdi mdi-plus sm-mr-1"></i>
           <span>Add User</span>
         </n-link>
       </div>
     </nav>
-  </div>
+
+    <div class="sm-mt-8">
+      <el-table :data="users"
+                style="width: 100%">
+        <el-table-column prop="date"
+                         width="40"/>
+        <el-table-column prop="id"
+                         label="id"/>
+        <el-table-column prop="name"
+                         label="Name"/>
+        <el-table-column prop="email"
+                         label="Email"/>
+        <el-table-column prop="confirm"
+                         label="Confirm">
+          <template slot-scope="scope">
+            <i v-if="scope.row.confirm"
+               class="mdi mdi-check sm-color-green"></i>
+            <i v-else
+               class="mdi mdi-close sm-color-red"></i>
+          </template>
+        </el-table-column>
+        <el-table-column prop="timeZone"
+                         label="Time Zone"/>
+        <el-table-column prop="role"
+                         label="Role"/>
+        <el-table-column prop="updatedAt"
+                         label="Updated"/>
+      </el-table>
+    </div>
+  </section>
 </template>
 
 <script>
