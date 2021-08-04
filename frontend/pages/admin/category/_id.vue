e1d008124b0a31c7f93e4774209cdf71ce1f037f
Smirnov <soe190715@gmail.com>
2021-08-04 14:59:22 +0700

    ui

--- frontend/pages/admin/category/_id.vue
+++ frontend/pages/admin/category/_id.vue
@@ -1,48 +1,71 @@
 <template>
-  <v-card>
-    <v-app-bar flat dense>
-      <v-btn text exact class="mx-2"
-             to="/admin/category">
-        <v-icon left>mdi-close</v-icon>
-        Cancel
-      </v-btn>
-      <v-btn text exact class="mx-2"
+  <section>
+    <nav class="sm-nav sm-bg-color-10 sm-color-color-9">
+      <div class="sm-nav-start">
+        <n-link to="/admin/category"
+                class="sm-nav-item sm-p-4 sm-link-hover sm-hover-color-10 sm-hover-bg-color-9">
+          <i class="mdi mdi-close sm-mr-1"></i>
+          <span>Выход</span>
+        </n-link>
+        <div class="sm-nav-item sm-p-4 sm-link sm-hover-color-10 sm-hover-bg-color-9"
              @click="store(false)">
-        <v-icon left>mdi-content-save</v-icon>
-        Save
-      </v-btn>
-      <v-btn text exact class="mx-2"
+          <i class="mdi mdi-content-save sm-mr-1"></i>
+          <span>Сохранить</span>
+        </div>
+        <div class="sm-nav-item sm-p-4 sm-link sm-hover-color-10 sm-hover-bg-color-9"
              @click="store(true)">
-        <v-icon left>mdi-content-save-move</v-icon>
-        Save and Close
-      </v-btn>
-    </v-app-bar>
+          <i class="mdi mdi-content-save-move sm-mr-1"></i>
+          <span>Сохранить и выйти</span>
+        </div>
+      </div>
+    </nav>
 
-    <v-container fluid>
-      <v-row>
-        <v-col cols="4">
-          <v-text-field
-              v-model="category.ru"
-              label="Category name RU"
-              :error-messages="errors['ru']"
-              required/>
-          <v-text-field
-              v-model="category.en"
-              label="Category name EN"
-              :error-messages="errors['en']"
-              required/>
-          <v-text-field
-              v-model="category.icon"
-              label="MDI icon"
-              :error-messages="errors['icon']"
-              required/>
-          <v-checkbox
-              v-model="category.active"
-              label="Active"/>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-card>
+    <div class="sm-mt-8 sm-wpx-400 sm-flex col">
+      <p class="sm-mb-1 sm-color-dark">
+        Имя RU
+      </p>
+      <div class="sm-form-input">
+        <el-input placeholder="Имя RU"
+                  v-model="category.ru"/>
+        <div class="sm-form-error"
+             v-html="$messageToStr(errors.ru)">
+        </div>
+      </div>
+
+      <p class="sm-mt-8 sm-mb-1 sm-color-dark">
+        Имя EN
+      </p>
+      <div class="sm-form-input">
+        <el-input placeholder="Имя EN"
+                  v-model="category.en"/>
+        <div class="sm-form-error"
+             v-html="$messageToStr(errors.en)">
+        </div>
+      </div>
+
+      <p class="sm-mt-8 sm-mb-1 sm-color-dark">
+        Иконка
+      </p>
+      <div class="sm-form-input">
+        <el-input placeholder="Иконка"
+                  v-model="category.icon"/>
+        <div class="sm-form-error"
+             v-html="$messageToStr(errors.icon)">
+        </div>
+      </div>
+
+      <p class="sm-mt-8 sm-mb-1 sm-color-dark">
+        Активный
+      </p>
+      <div class="sm-form-input">
+        <el-switch v-model="category.active"/>
+        <div class="sm-form-error"
+             v-html="$messageToStr(errors.active)">
+        </div>
+      </div>
+    </div>
+
+  </section>
 </template>
 
 <script>
