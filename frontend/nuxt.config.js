ffcbc5e2a02444908a0d2c0feae80b519648c836
Smirnov <soe190715@gmail.com>
2021-08-19 13:08:42 +0700

    add mail

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -8,8 +8,7 @@ export default {
         meta         : [
             {charset: 'utf-8'},
             {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-            {hid: 'description', name: 'description', content: ''},
-            {name: 'mailru-domain', content: 'HPz2i5mdR0UJqUc5'}
+            {hid: 'description', name: 'description', content: ''}
         ],
         link         : [
             {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
