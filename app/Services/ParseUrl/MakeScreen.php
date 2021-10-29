403ea86f34ba29fb3e26fecdd74bafa9d0ff78a7
Smirnov <soe190715@gmail.com>
2021-10-29 20:45:48 +0700

    add user_desc

--- app/Services/ParseUrl/MakeScreen.php
+++ app/Services/ParseUrl/MakeScreen.php
@@ -28,8 +28,13 @@ class MakeScreen
             $screenCapture = new Capture($url);
             $screenCapture->binPath = '/usr/local/bin/';
             $screenCapture->setOptions([
+                'ssl-protocol'      => 'any',
                 'ignore-ssl-errors' => true
             ]);
+            $screenCapture->setWidth(1100);
+            $screenCapture->setHeight(700);
+            $screenCapture->setImageType(Png::FORMAT);
+            $screenCapture->setUserAgentString('Mozilla/5.0 (X11; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0');
             $screenCapture->save($fullPath);
         } catch (PhantomJsException $e) {
             Log::error('Error make screen', [$e->getMessage()]);
