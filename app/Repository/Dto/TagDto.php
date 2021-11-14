6c759aa602646ffe21bf638034adeb3ad60711d7
soeSmile <soe190715@gmail.com>
2021-11-15 04:31:36 +0700

    #7 mb_strtolower

--- app/Repository/Dto/TagDto.php
+++ app/Repository/Dto/TagDto.php
@@ -3,11 +3,22 @@ declare(strict_types=1);
 
 namespace App\Repository\Dto;
 
+use App\Repository\AbstractRepository;
+
 /**
  * Class TagDto
  * @package App\Repository\Dto
  */
 final class TagDto extends AbstractDto
 {
+    /**
+     * @param AbstractRepository|null $abstractRepository
+     * @return array
+     */
+    public function getData(?AbstractRepository $abstractRepository = null): array
+    {
+        $this->setDataByKey('name', \mb_strtolower($this->getDataByKey('name')));
 
+        return parent::getData($abstractRepository);
+    }
 }
